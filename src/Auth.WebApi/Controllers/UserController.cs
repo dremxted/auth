8c22061b05be0f570bf0fdc6c0f1dee4dc6f31dc
unknown <valerii.okhmaniuk@gmail.com>
2026-03-23 00:48:48 +0100

    Add UserController.cs

---
+++ src/Auth.WebApi/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Auth.WebApi.Controllers;
+
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet("api/current")]
+    public async Task<IActionResult> GetUser()
+    {
+        var name = HttpContext.User
+            .FindFirst(ClaimTypes.Name);
+
+        return name != null
+            ? Ok($"Hello, {name.Value}!")
+            : Unauthorized();
+    }
+}
