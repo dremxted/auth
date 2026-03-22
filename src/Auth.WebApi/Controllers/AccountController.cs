4b9c30b455d4d246054109157dca0491e719a105
unknown <valerii.okhmaniuk@gmail.com>
2026-03-23 00:48:27 +0100

    Add AccountController.cs

---
+++ src/Auth.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Auth.WebApi.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Auth.WebApi.Controllers;
+
+[ApiController]
+[AllowAnonymous]
+public class AccountController : ControllerBase
+{
+    [HttpPost("api/login")]
+    public async Task LoginAsync(LoginRequestDto loginRequestDto)
+    {
+        var claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.Name, loginRequestDto.Username),
+        };
+
+        var claimsIdentity = new ClaimsIdentity(
+            claims: claims,
+            authenticationType: CookieAuthenticationDefaults.AuthenticationScheme,
+            nameType: ClaimTypes.Name,
+            roleType: default);
+
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+        await HttpContext.SignInAsync(
+            scheme: CookieAuthenticationDefaults.AuthenticationScheme,
+            principal: claimsPrincipal);
+    }
+
+    [HttpPost("api/logout")]
+    public async Task LogoutAsync()
+    {
+        await HttpContext.SignOutAsync(
+            scheme: CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+}
