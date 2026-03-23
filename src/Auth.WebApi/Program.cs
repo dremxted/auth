e0566fac9ec3c356b6a1935093151f4a78fc05ec
unknown <valerii.okhmaniuk@gmail.com>
2026-03-23 22:56:05 +0100

    Update Program.cs to configure google authentication and authorization

--- src/Auth.WebApi/Program.cs
+++ src/Auth.WebApi/Program.cs
@@ -1,22 +1,38 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+var googleSection = config.GetSection("Google");
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(authenticationScheme: CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddGoogle(options =>
+    {
+        options.ClientId = googleSection["ClientId"]!;
+        options.ClientSecret = googleSection["ClientSecret"]!;
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    });
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference(options => 
+    app.MapScalarApiReference(options =>
     {
-        options.WithDefaultHttpClient(ScalarTarget.CSharp,ScalarClient.HttpClient);
+        options.WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
     });
 }
 
 app.UseHttpsRedirection();
-app.MapControllers();
+
+app.UseAuthentication();
+app.UseAuthentication();
+
+app.MapControllers()
+    .RequireAuthorization();
 
 app.Run();
