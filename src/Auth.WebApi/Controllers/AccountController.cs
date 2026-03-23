ce131f5757a23b26895a50b861b8c0431bff8c51
unknown <valerii.okhmaniuk@gmail.com>
2026-03-23 23:03:18 +0100

    Add AccountController.cs

---
+++ src/Auth.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.WebApi.Controllers;
+
+[ApiController]
+[AllowAnonymous]
+public class AccountController : ControllerBase
+{
+    [HttpGet("api/login/google")]
+    public IActionResult Login(string returnUrl)
+    {
+        if(!Url.IsLocalUrl(returnUrl))
+        {
+            return BadRequest();
+        }
+
+        AuthenticationProperties authenticationProperties = new()
+        {
+            RedirectUri = returnUrl
+        };
+
+        return Challenge(
+            authenticationProperties, 
+            GoogleDefaults.AuthenticationScheme);
+    }
+}
