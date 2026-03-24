d44648e33998f097dd874ff474bc33f70bcf7fdc
unknown <valerii.okhmaniuk@gmail.com>
2026-03-24 19:51:41 +0100

    Add UserController.cs

---
+++ src/Auth.WebApi/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.WebApi.Controllers;
+
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet("api/current")]
+    public IActionResult GetUser()
+    {
+        Dictionary<string,string> claims = new();
+        
+        foreach(var claim in HttpContext.User.Claims)
+        {
+            claims.Add(claim.Type, claim.Value);
+        }
+
+        return Ok(claims);
+    }
+}
