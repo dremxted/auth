0d8cf4af2e9144e6d7eef510d7a0a6fe487a173c
unknown <valerii.okhmaniuk@gmail.com>
2026-03-26 23:20:35 +0100

    Add GetAccessToken endpoint to UserController.cs

--- src/Auth.WebApi/Controllers/UserController.cs
+++ src/Auth.WebApi/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Auth.WebApi.Controllers;
 
@@ -17,4 +18,12 @@ public class UserController : ControllerBase
 
         return Ok(claims);
     }
+
+    [HttpGet("api/accesstoken")]
+    public async Task<IActionResult> GetAccessToken()
+    {
+        string? token = await HttpContext.GetTokenAsync("access_token");
+        return Ok(token);
+    }
+
 }
