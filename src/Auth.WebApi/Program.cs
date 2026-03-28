4b20f134d3dcea07bc912b0b46ec02aaa7c01990
unknown <valerii.okhmaniuk@gmail.com>
2026-03-29 00:34:38 +0100

    Update Program.cs to configure oidc authentication and authorization

--- src/Auth.WebApi/Program.cs
+++ src/Auth.WebApi/Program.cs
@@ -1,9 +1,41 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+var authConfig = config.GetSection("Auth0");
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+    .AddCookie()
+    .AddOpenIdConnect(
+    authenticationScheme: OpenIdConnectDefaults.AuthenticationScheme,
+    configureOptions: options => 
+    {
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.ResponseType = OpenIdConnectResponseType.Code;
+        options.MapInboundClaims = false;
+        //options.SaveTokens = true;
+        options.UsePkce = true;
+
+        //options.CallbackPath = "/signin-oidc";
+        options.Authority = authConfig["Authority"];
+        options.ClientId = authConfig["ClientId"];
+        options.ClientSecret = authConfig["ClientSecret"];
+
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+    });
 
 var app = builder.Build();
 
@@ -17,6 +49,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.MapControllers();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers()
+    .RequireAuthorization();
 
 app.Run();
