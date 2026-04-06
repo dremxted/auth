cca7f0cdd5d5433332d8c8da7c66a3e298681e50
unknown <valerii.okhmaniuk@gmail.com>
2026-04-06 14:34:37 +0200

    Add AccountController.cs

---
+++ src/Auth.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿namespace Auth.WebApi.Controllers;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[AllowAnonymous]
+public class AccountController : ControllerBase
+{
+    [HttpGet("api/login")]
+    public IActionResult LoginOpenIddict(string returnUrl)
+    {
+        if (!Url.IsLocalUrl(returnUrl))
+        {
+            return BadRequest();
+        }
+
+        AuthenticationProperties authenticationProperties = new()
+        {
+            RedirectUri = returnUrl
+        };
+
+        return Challenge(
+            authenticationProperties,
+            "EntraId");
+    }
+}
