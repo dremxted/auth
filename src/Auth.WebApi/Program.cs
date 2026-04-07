1320f9ac5308ffbe622b889fcdde4bed042e6ccf
unknown <valerii.okhmaniuk@gmail.com>
2026-04-07 02:01:36 +0200

    Update Program.cs to configure oidc authentication and authorization

--- src/Auth.WebApi/Program.cs
+++ src/Auth.WebApi/Program.cs
@@ -1,22 +1,52 @@
-using Scalar.AspNetCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+var entraConfig = config.GetSection("EntraId");
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = "EntraId";
+})
+    .AddCookie()
+    .AddOpenIdConnect(
+    authenticationScheme: "EntraId",
+    configureOptions: options =>
+    {
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.ResponseType = OpenIdConnectResponseType.Code;
+        options.MapInboundClaims = false;
+        options.UsePkce = true;
+
+        options.CallbackPath = "/signin-entra";
+        options.Authority = entraConfig["Authority"];
+        options.ClientId = entraConfig["ClientId"];
+        options.ClientSecret = entraConfig["ClientSecret"];
+
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+    });
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference(options => 
-    {
-        options.WithDefaultHttpClient(ScalarTarget.CSharp,ScalarClient.HttpClient);
-    });
 }
 
 app.UseHttpsRedirection();
-app.MapControllers();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers()
+    .RequireAuthorization();
 
 app.Run();
